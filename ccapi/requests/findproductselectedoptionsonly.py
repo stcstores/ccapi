f894cc545a907ecbad383faa8b0e84b9ff579478
Luke Shiner <luke@lukeshiner.com>
2017-06-05 16:52:10 +0100

    Gets location from call to FindProductSelectedOptionsOnly

--- ccapi/requests/findproductselectedoptionsonly.py
+++ ccapi/requests/findproductselectedoptionsonly.py
@@ -65,8 +65,27 @@ class Product:
         self.product_template_id = data['ProductTemplateId']
         self.product_template_mode = data['ProductTemplateMode']
         self.additional_barcodes = data['AdditionalBarcodes']
-        self.locations = data['Locations']
+        self.locations = [Location(location) for location in data['Locations']]
         self.dimensions = data['Dimensions']
+
+
+class Location:
+
+    def __init__(self, data):
+        self.id = data['ID']
+        self.warehouse_id = data['WarehouseID']
+        self.name = data['Name']
+        self.bay_number = data['BayNumber']
+        self.aisle = data['Aisle']
+        self.shelf = data['Shelf']
+        self.available_stock = data['AvailableStock']
+        self.warehouse_bay_type = data['WarehouseBayType']
+        self.warehouse_bay_type_enum = data['WarehouseBayTypeEnum']
+        self.status_id = data['StatusId']
+        self.status_id_enum = data['StatusIdEnum']
+
+    def __repr__(self):
+        return self.name
 
 
 class Option:
