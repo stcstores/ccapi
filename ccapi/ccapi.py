bdd247e2ae30217884e680e48bfecdc06996c1d4
Luke Shiner <luke@lukeshiner.com>
2017-06-06 13:57:42 +0100

    Make CCAPI methods static

--- ccapi/ccapi.py
+++ ccapi/ccapi.py
@@ -5,25 +5,34 @@ from . import requests
 class CCAPI:
 
     def __init__(self, username, password):
+        self.create_session(username, password)
+
+    @staticmethod
+    def create_session(username, password):
         CloudCommerceAPISession.get_session(username, password)
 
-    def search_products(self, search_text):
+    @staticmethod
+    def search_products(search_text):
         return requests.DoSearch(search_text)
 
-    def get_sku(self, range_sku=False):
+    @staticmethod
+    def get_sku(range_sku=False):
         response = requests.ProductOperations('getgeneratedsku')
         sku = response.data
         if range_sku is True:
             sku = 'RNG_{}'.format(sku)
         return sku
 
-    def get_variation_by_id(self, variation_id):
+    @staticmethod
+    def get_variation_by_id(variation_id):
         return requests.FindProductSelectedOptionsOnly(variation_id)
 
-    def get_product_options(self):
+    @staticmethod
+    def get_product_options():
         return requests.GetOptions()
 
+    @staticmethod
     def update_product_stock_level(
-            self, product_id, new_stock_level, old_stock_level):
+            product_id, new_stock_level, old_stock_level):
         requests.UpdateProductStockLevel(
             str(product_id), new_stock_level, old_stock_level)
