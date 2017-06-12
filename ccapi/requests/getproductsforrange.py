4be4cf3274bf47c6e1f76bc9d3443df974fc30b2
Luke Shiner <luke@lukeshiner.com>
2017-06-12 17:16:19 +0100

    Add docstrings to getproductsforrange.py

--- ccapi/requests/getproductsforrange.py
+++ ccapi/requests/getproductsforrange.py
@@ -1,22 +1,38 @@
+"""
+GetProductsForRange request.
+
+Gets information about a given product range.
+"""
+
 from . apirequest import APIRequest
 from .. inventoryitems import ProductRange
 
 
 class GetProductsForRange(APIRequest):
+    """GetProductsForRange request."""
+
     uri = 'Handlers/getProductsForRange.ashx'
 
     def __new__(self, product_id):
+        """Create GetProductsForRange request.
+
+        Args:
+            product_id: ID of range.
+        """
         self.product_id = product_id
         return super().__new__(self)
 
     def process_response(self, response):
+        """Handle request response."""
         result = response.json()
         return ProductRange(result)
 
     def get_data(self):
+        """Get data for request."""
         return {
             'ProdRangeID': self.product_id,
             'salesChannelID': "0"}
 
     def get_params(self):
+        """Get parameters for get request."""
         return {'d': '155'}
