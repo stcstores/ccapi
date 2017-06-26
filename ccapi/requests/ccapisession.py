aae22ab4614fbdc2afb763fe5b38bc68b49ede7f
Luke Shiner <luke@lukeshiner.com>
2017-06-26 11:11:35 +0100

    Add timeout to session.

--- ccapi/requests/ccapisession.py
+++ ccapi/requests/ccapisession.py
@@ -1,5 +1,7 @@
 """This module contains the CloudCommerceAPISession class."""
 
+from datetime import datetime, timedelta
+
 import requests
 from urllib.parse import urljoin
 
@@ -9,20 +11,31 @@ class CloudCommerceAPISession:
 
     domain = 'http://seatontradingcompany.cloudcommercepro.com'
     login_url = domain
+    timeout = timedelta(hours=12)
 
     @classmethod
     def get_session(cls, username, password):
         """Create logged in session with Cloud Commerce."""
+        cls.username = username
+        cls.password = password
         cls.session = requests.Session()
         login_post_data = {
             'usernameInput': username, 'passwordInput': password}
         cls.session.post(cls.login_url, data=login_post_data)
+        cls.last_login = datetime.now()
 
     @classmethod
     def api_request(cls, request):
         """Perform API request."""
+        cls.check_login()
         url = urljoin(cls.domain, request.uri)
         response = cls.session.post(
             url, headers=request.headers, params=request.params,
             data=request.data)
         return response
+
+    @classmethod
+    def check_login(cls):
+        """Get new session if current session has expired."""
+        if cls.last_login + cls.timeout < datetime.now():
+            cls.get_session(cls.username, cls.password)
