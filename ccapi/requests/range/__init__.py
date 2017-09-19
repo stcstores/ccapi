578f6a4c51c1dc4e9b1c8415ea52394d5bbcd065
Luke Shiner <luke@lukeshiner.com>
2017-09-19 12:26:52 +0100

    Fix missnamed checkrangeonsaleschannel.py

--- ccapi/requests/range/__init__.py
+++ ccapi/requests/range/__init__.py
@@ -1,7 +1,7 @@
 from . addnewrange import AddNewRange  # noqa
 from . addremproductoption import AddRemProductOption  # noqa
 from . deleteproductrange import DeleteProductRange  # noqa
-from . chackrangesonsaleschannel import CheckRangesOnSalesChannel  # noqa
+from . checkrangesonsaleschannel import CheckRangesOnSalesChannel  # noqa
 from . setoptionselect import SetOptionSelect  # noqa
 from . updateonsaleschannel import UpdateOnSalesChannel  # noqa
 from . updaterangesettings import UpdateRangeSettings  # noqa
