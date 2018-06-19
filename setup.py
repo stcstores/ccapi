3d686a15ebf3bf10988c4acf3c597cc17880b984
Luke Shiner <luke@lukeshiner.com>
2018-06-19 14:55:20 +0100

    Use __version__.py file.

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python
-"""Setup for the ccapi package."""
+"""Setup for CCAPI."""
 
-from setuptools import find_packages, setup
+import os
 
-setup(
-    name='ccapi',
-    version='0.0.1',
-    description='Cloud Commerce Pro API integration.',
-    author='Luke Shiner',
-    author_email='luke@lukeshiner.com',
+import setuptools
+
+NAME = 'ccapi'
+
+with open("README.rst", "r") as readme:
+    long_description = readme.read()
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, NAME, '__version__.py'), 'r') as f:
+    exec(f.read(), about)
+
+setuptools.setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=long_description,
+    url=about['__url__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
     install_requires=['requests', 'beautifulsoup4'],
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
 )
