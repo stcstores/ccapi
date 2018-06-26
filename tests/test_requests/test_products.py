4e2a68c0cb7f944c554afb138a7caaa38309f8c2
Luke Shiner <luke@lukeshiner.com>
2018-06-26 14:00:59 +0100

    Add tests for doSearch requests.

--- tests/test_requests/test_products.py
+++ tests/test_requests/test_products.py
@@ -5,6 +5,47 @@ from ccapi.requests import products
 from .test_request import TestRequest
 
 
+class TestDoSearch(TestRequest):
+    """Tests for doSearch requests."""
+
+    request_class = products.DoSearch
+
+    RESULT_ID = '4347654'
+    SKU_SEARCH_TEXT = 'WUA-DU7-W6W'
+    RESULT_VARIATION_ID = '6909316'
+    RESULT_NAME = "Product Editor Test Variations Updated Title |  4XL"
+    RESULT_THUMBNAIL = "//image.png"
+    RESULT_DESCRIPTION = "<p>"
+
+    SUCCESSFUL_RESPONSE = [
+        {
+            "ID": RESULT_ID,
+            "variationID": RESULT_VARIATION_ID,
+            "Name": RESULT_NAME,
+            "SKU": SKU_SEARCH_TEXT,
+            "Description": RESULT_DESCRIPTION,
+            "Thumbnail": RESULT_THUMBNAIL,
+        }
+    ]
+    EMPTY_RESPONSE = []
+
+    def test_product_search(self):
+        """Test searching for a SKU."""
+        self.register(json=self.SUCCESSFUL_RESPONSE)
+        response = self.mock_request(self.SKU_SEARCH_TEXT)
+        self.assertEqual(response[0].id, self.RESULT_ID)
+        self.assertEqual(response[0].variation_id, self.RESULT_VARIATION_ID)
+        self.assertEqual(response[0].name, self.RESULT_NAME)
+        self.assertEqual(response[0].sku, self.SKU_SEARCH_TEXT)
+        self.assertEqual(response[0].thumbnail, self.RESULT_THUMBNAIL)
+
+    def test_unmatched_search(self):
+        """Test a search with no results."""
+        self.register(json=self.EMPTY_RESPONSE)
+        response = self.mock_request('This search will not match anything')
+        self.assertEqual(response, [])
+
+
 class TestProductOperations(TestRequest):
     """Tests for the ProductOperations request."""
 
