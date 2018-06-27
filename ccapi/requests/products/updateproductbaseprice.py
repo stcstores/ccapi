26696059530150459f13f85e4cb0d09667c628a1
Luke Shiner <luke@lukeshiner.com>
2018-06-27 15:19:11 +0100

    Error handling for the UpdateProductBasePrice request.

--- ccapi/requests/products/updateproductbaseprice.py
+++ ccapi/requests/products/updateproductbaseprice.py
@@ -1,6 +1,5 @@
 """updateProductBasePrice Request."""
 
-
 from ..apirequest import APIRequest
 
 
@@ -13,7 +12,7 @@ class UpdateProductBasePrice(APIRequest):
 
     uri = 'Handlers/Products/updateProductBasePrice.ashx'
 
-    def __new__(self, product_id, price):
+    def __new__(self, *, product_id, price):
         """
         Create updateProductBasePrice request.
 
@@ -27,13 +26,15 @@ class UpdateProductBasePrice(APIRequest):
 
     def process_response(self, response):
         """Handle request response."""
+        self.raise_for_non_200(
+            self, response,
+            'Error setting base price for product with ID "{}".'.format(
+                self.product_id))
         return response.text
 
     def get_data(self):
         """Get data for request."""
-        return {
-            'prodid': self.product_id,
-            'price': self.price}
+        return {'prodid': self.product_id, 'price': self.price}
 
     def get_params(self):
         """Get parameters for get request."""
