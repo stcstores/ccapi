fe4003f187d6bb227518eb1eed3b700838afe806
Luke Shiner <luke@lukeshiner.com>
2018-07-12 11:18:48 +0100

    Test ProductBarcodeInUse sends the correct barcode.

--- tests/test_requests/test_product_barcode.py
+++ tests/test_requests/test_product_barcode.py
@@ -9,6 +9,7 @@ class TestProductBarcodeInUse(TestRequest):
     """Tests for the ProductBarcodeInUse request."""
 
     request_class = productbarcode.ProductBarcodeInUse
+    BARCODE = '13245679812'
     UNUSED_RESPONSE = {
         'Success': False,
         'Message': 'Barcode not in use',
@@ -22,14 +23,20 @@ class TestProductBarcodeInUse(TestRequest):
         'Data': [],
     }
 
+    def test_ProductBarcodeInUse_request(self):
+        """Test the ProductBarcodeInUse request."""
+        self.register(json=self.UNUSED_RESPONSE)
+        self.mock_request(self.BARCODE)
+        self.assertDataSent('BarcodeNumber', self.BARCODE)
+
     def test_with_unused_barcode(self):
         """Test reqeuest gives correct response for an unused barcode."""
         self.register(json=self.UNUSED_RESPONSE)
-        response = self.mock_request('13245679812')
+        response = self.mock_request(self.BARCODE)
         self.assertFalse(response)
 
     def test_with_used_barcode(self):
         """Test reqeuest gives correct response for a used barcode."""
         self.register(json=self.USED_RESPONSE)
-        response = self.mock_request('13245679812')
+        response = self.mock_request(self.BARCODE)
         self.assertTrue(response)
