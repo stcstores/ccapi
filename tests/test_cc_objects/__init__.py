28d01ea19dc347e7bfbdb6c8072bd64f83b1c870
Luke Shiner <luke@lukeshiner.com>
2018-07-17 12:09:27 +0100

    Add structrure for testing the cc_objects module.

---
+++ tests/test_cc_objects/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for the ccapi.cc_objects module."""
---
+++ tests/test_cc_objects/test_cc_objects.py
@@ -0,0 +1,9 @@
+"""Tests for the ccapi.cc_objects module."""
+
+from ..test_CCAPI import TestCCAPI
+
+
+class TestCCObjects(TestCCAPI):
+    """Base test class for cc_objects tests."""
+
+    pass
