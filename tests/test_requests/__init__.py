127cd240240a79881c67d0071f0a037828f48551
Luke Shiner <luke@lukeshiner.com>
2018-09-18 15:46:08 +0100

    Add tests for the CreatePayment request

--- tests/test_requests/__init__.py
+++ tests/test_requests/__init__.py
@@ -5,3 +5,4 @@ from .test_product_barcode import *  # NOQA
 from .test_range import *  # NOQA
 from .test_handlers import *  # NOQA
 from .test_program_type_requests import *  # NOQA
+from .test_accounts import *  # NOQA
